43ae9de5c3e954edbf5864280ef6f95b292dc06f
Mami Ravaloarison <maminiainaravaloarison@gmail.com>
2026-02-19 12:08:37 -0500

    Initial Commit

---
+++ CS193p-2025/CS193p_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_2025App.swift
+//  CS193p-2025
+//
+//  Created by Mami RavaLoarison on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p-2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CS193p-2025
+//
+//  Created by Mami RavaLoarison on 2/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
